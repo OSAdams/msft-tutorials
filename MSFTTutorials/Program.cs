f576761828b4fe17508b1b1abd8c85453d8b0209
Owen Adams <falcocodes@protonmail.ch>
2025-10-14 16:33:17 -0400

    Console.Write and Console.WriteLine

--- MSFTTutorials/Program.cs
+++ MSFTTutorials/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var VariableName = "Hello World!";
+Console.WriteLine(VariableName);
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.WriteLine("You wrote your first lines of code.");
+Console.WriteLine("Congratulations!");
+Console.WriteLine("You wrote your first lines of code.");
+Console.WriteLine("This is the first line.");
+Console.WriteLine(VariableName);
+Console.WriteLine("This is the second line.");
+Console.WriteLine("==============================");
+// Console.Write('Single Quotes.');
