158a328a348948b2de3763d51196aff20d9127aa
Owen Adams <falcocodes@protonmail.ch>
2025-10-15 12:04:28 -0400

    Basic C# tutorial WIP

--- MSFTTutorials/Program.cs
+++ MSFTTutorials/Program.cs
@@ -1,13 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var VariableName = "Hello World!";
-Console.WriteLine(VariableName);
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.WriteLine("You wrote your first lines of code.");
-Console.WriteLine("Congratulations!");
-Console.WriteLine("You wrote your first lines of code.");
-Console.WriteLine("This is the first line.");
-Console.WriteLine(VariableName);
-Console.WriteLine("This is the second line.");
-Console.WriteLine("==============================");
+// var VariableName = "Hello World!";
+// Console.WriteLine(VariableName);
+// Console.Write("Congratulations!");
+// Console.Write(" ");
+// Console.WriteLine("You wrote your first lines of code.");
+// Console.WriteLine("Congratulations!");
+// Console.WriteLine("You wrote your first lines of code.");
+// Console.WriteLine("This is the first line.");
+// Console.WriteLine(VariableName);
+// Console.WriteLine("This is the second line.");
+// Console.WriteLine("==============================");
 // Console.Write('Single Quotes.');
+
+// Console.WriteLine(123);
+// Console.WriteLine(0.25F);
+// Console.WriteLine(2.625);
+// Console.WriteLine(12.39816m);
+// Console.WriteLine(true);
+// Console.WriteLine(false);
+// Console.WriteLine("=============================");
+
+// string firstName;
+// firstName = "Bob";
+// string lastName = "Saget";
+// Console.Write("Your first name is ");
+// Console.Write(firstName);
+// Console.Write("! Your last name is ");
+// Console.Write(lastName);
+// Console.WriteLine("!");
+
+// firstName = "Liem";
+// Console.WriteLine(firstName);
+// firstName = "Isabella";
+// Console.WriteLine(firstName);
+// firstName = "Yasmin";
+// Console.WriteLine(firstName);
+
+// string errorTime;
+// Console.WriteLine(errorTime);
+
+string firstName = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(firstName);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.WriteLine(" celcius.");
+Console.Write(34.40m);
