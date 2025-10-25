decc7343e869c9169b5c26ad2a55c619b6c936eb
Owen Adams <falcocodes@protonmail.ch>
2025-10-25 16:07:34 -0400

    Complete tutorial

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//int a = 7;
+//int b = 4;
+//int c = 3;
+//int d = (a + b) / c;
+//int e = (a + b) % c;
+//Console.WriteLine($"quotient: {d}");
+//Console.WriteLine($"remainder: {e}");
+
+//int max = int.MaxValue;
+//int min = int.MinValue;
+//Console.WriteLine($"The range of integers is {min} to {max}");
+
+//int what = max + 3;
+//Console.WriteLine($"An example of overflow: {what}");
+
+//double max = double.MaxValue;
+//double min = double.MinValue;
+//Console.WriteLine($"The range of double is {min} to {max}");
+
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
